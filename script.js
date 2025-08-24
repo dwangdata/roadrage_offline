dd3ae637f40714ee1589b8f631c4b48f03277226
Rupin Dalvi <rdalvi@arcresources.com>
2025-08-24 17:14:52 -0600

    Change bell sound timing

--- script.js
+++ script.js
@@ -357,8 +357,7 @@ function playBell() {
   // Double ring
   try {
     bellSound.currentTime = 0;
-    bellSound.play();
-      /*.then(() => {
+    bellSound.play().then(() => {
       setTimeout(() => {
         try {
           bellSound.currentTime = 0;
@@ -644,6 +643,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (closeDetailButton)closeDetailButton.addEventListener('click', hideRideDetails);
   if (bellButton)       bellButton.addEventListener('click', window.playBell);
 });
+
 
 
 
